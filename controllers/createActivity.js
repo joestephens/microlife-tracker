2fa055f569810b92b61a4abaa36f7c821e66627f
Joe Stephens <joe@webantic.co.uk>
2018-04-09 13:28:38 +0100

    Simplify for walkthrough

--- controllers/createActivity.js
+++ controllers/createActivity.js
@@ -3,20 +3,21 @@ const path = require('path');
 const uuid = require('uuid/v5');
 
 const createActivity = (req, res) => {
-  const filePath = path.join(__dirname, 'profile.json');
+  const filePath = path.join(__dirname, 'user.json');
 
   fs.readFile(filePath, 'utf8', (readError, userJson) => {
     if (readError) throw readError;
 
     const user = JSON.parse(userJson);
 
-    const activity = Object.assign(req.body, { _id: uuid() });
+    const activityId = uuid()
+    const activity = Object.assign({}, req.body, { _id: activityId });
     user.profile.activities.push(activity);
 
     fs.writeFile(filePath, JSON.stringify(user), (writeError) => {
       if (writeError) throw writeError;
 
-      res.status(200).send({ profileActivityId: activity._id });
+      res.status(200).send({ profileActivityId: activityId });
     });
   });
 };
